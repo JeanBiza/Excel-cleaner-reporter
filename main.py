4c5af2225571a1c14f5624319f8984cc3ee338d1
JeanBiza <jampybizama21@gmail.com>
2026-04-10 13:01:16 -0400

    better main.py

--- main.py
+++ main.py
@@ -7,12 +7,11 @@ if __name__ == "__main__":
     if len(files) == 0:
         print("No hay archivos Excel en la carpeta input/ para procesar")
     elif len(files) == 1:
+        print(f"{"-" * 20} Procesando archivo : {files[0].name} {"-" * 20}")
         df = clean_excel(str(files[0]))
-        print(df)
         df = auto_clean(df)
         name = files[0].name
         df.to_excel(f"output/clean_{name}", index=False)
-        print(df)
     else:
         for i, file in enumerate(files):
             print(f"{i+1}. {file.name}")
@@ -24,13 +23,15 @@ if __name__ == "__main__":
             exit()
 
         if option == 0:
-            for i in files:
-                df = clean_excel(str(i))
+            for i, file in enumerate(files):
+                print(f"{"-"*20} Procesando archivo N°{i+1} : {file.name} {"-"*20}")
+                df = clean_excel(str(file))
                 df = auto_clean(df)
-                name = i.name
+                name = file.name
                 df.to_excel(f"output/clean_{name}", index=False)
         else:
             file = files[option - 1]
+            print(f"{"-" * 20} Procesando archivo : {file.name} {"-" * 20}")
             df = clean_excel(str(file))
             df = auto_clean(df)
             name = file.name
